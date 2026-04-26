5713ed76e0eb534139dd6f5c5c0f6fe0563fee1e
Dinfadashe <dinfadashe@gmail.com>
2026-04-26 11:22:47 +0100

    Fix: remove duplicate exports from pages index

--- apps/web/components/pages/index.ts
+++ apps/web/components/pages/index.ts
@@ -1,7 +1,5 @@
-export { default as ExplorePage } from './OtherPages';
-export { ExplorePage } from './OtherPages';
-export { default as TokenomicsPage } from './OtherPages';
 export { TokenomicsPage } from './OtherPages';
+export { ExplorePage } from './OtherPages';
 export { AnalyticsPage } from './OtherPages';
 export { LibraryPage } from './OtherPages';
 export { LikedPage } from './OtherPages';
